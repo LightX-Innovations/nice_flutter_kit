048e9840c289cf12d0d76515eba358d6cceded85
Nicolas Depelteau <nicolas@depelteau.ca>
2024-07-08 12:04:14 -0400

    feat: Add needSubscription property to NiceFilterModel

--- lib/src/data-filter/models/filter.model.dart
+++ lib/src/data-filter/models/filter.model.dart
@@ -5,18 +5,23 @@ class NiceFilterModel {
   final NiceFilterPageModel page;
   final NiceFilterQueryModel? query;
   final NiceFilterSearchModel? search;
+  final bool? needSubscription;
 
   const NiceFilterModel({
     this.order,
     required this.page,
     this.query,
     this.search,
+    this.needSubscription,
   });
 
   Json toJson() => {
-        if (order != null) "order": order!.map((order) => order.toJson()).toList(growable: false),
+        if (order != null)
+          "order":
+              order!.map((order) => order.toJson()).toList(growable: false),
         "page": page.toJson(),
         if (query != null) "query": query!.toJson(),
         if (search != null) "search": search!.toJson(),
+        if (needSubscription != null) "needSubscription": needSubscription,
       };
 }
